ee15c133bd800021a1fe6e12749c60d9a2a95913
Comingsoon <comingsoon000@ya.ru>
2022-01-28 14:08:09 +0300

    server-04: implement update-product Dto

---
+++ src/products/dto/update-pruduct.dto.ts
@@ -0,0 +1,9 @@
+export class UpdateProductDto {
+  readonly name: string;
+  readonly price: number;
+  readonly unitMeasure: string;
+  readonly title: string;
+  readonly description: string;
+  readonly iconUrl: string;
+  readonly categories: string[];
+}
