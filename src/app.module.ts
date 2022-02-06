22f773bb4d6ecc44baa5731ab221217fd70d4768
Sergey Maksimov <s.mksimov@gmail.com>
2022-02-06 18:06:06 +0300

    server-10: add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import 'dotenv/config';
 
-const mongoURI = '';
+const mongoURI = process.env.DB_URI;
 
 @Module({
   imports: [ProductsModule, MongooseModule.forRoot(mongoURI)],
