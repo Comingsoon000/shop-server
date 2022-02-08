d421f5458e5dce59f39c48ab4ed6701399b52a81
Sergey Maksimov <s.mksimov@gmail.com>
2022-02-08 13:47:03 +0300

    auth: implement users controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAll(): Promise<User[]> {
+    return this.usersService.getAll();
+  }
+
+  @Get()
+  getById(@Param('id') id: string): Promise<User> {
+    return this.usersService.getById(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto)
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<User> {
+    return this.usersService.remove(id);
+  }
+
+}
+
