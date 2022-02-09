23c7a19fd84704a9c87ca4ba0518d91663bf4e1c
Comingsoon <comingsoon000@ya.ru>
2022-02-09 08:54:41 +0300

    categories: add import CategoriesModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from 'src/categories/categories.module';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product, ProductSchema } from './schemas/product.schema';
@@ -8,6 +9,7 @@ import { Product, ProductSchema } from './schemas/product.schema';
   providers: [ProductsService],
   controllers: [ProductsController],
   imports: [
+    CategoriesModule,
     MongooseModule.forFeature([{ schema: ProductSchema, name: Product.name }]),
   ],
 })
